15c4a698a12d2274bdef966c4402fcbe73a8bcf5
spa23 <stefan.avey@yale.edu>
2014-08-28 14:22:54 -0400

    na

--- R/aveytoolkit_collapseDataset.R
+++ R/aveytoolkit_collapseDataset.R
@@ -7,11 +7,11 @@
 #' @param mapVector a named character vector with names specififying the current identifiers (probes matching the rownames of exprsVals) and the values of the vector specifying the gene symbols (or other identifier to collapse to). 
 #' @param oper the operation used to choose which probe when multiple probes map to the same gene.  Default is max which will calculate the maximum of the average.
 #' @param prefer one of "none", "up", or "down", can be abbreviated.
-#' @param singleProbeset If \code{TRUE}, the operation applies to the average over all conditions and all values for a gene will come from one probeset. Otherwise, if \code{FALSE}, the operation applies to the probesets over all conditions and the values for a gene may come from different probe sets . Default is \code{FALSE} for compatability reasons but \code{TRUE} is recommended.
+#' @param singleProbeset If \code{TRUE}, the operation applies to the average of each sample. Otherwise, if \code{FALSE}, the operation applies to the probesets over all samples and only one probeset will be selected. Default is \code{FALSE}.
 #' @param returnProbes if \code{TRUE}, a list of the collapsed expression matrix and the probes are both returned (see return).
 #' @param deProbes a list with named vectors "up" and "down" giving the names of up and downregulated probes
 #' @return If returnProbes is \code{TRUE}, a list containing the collapsed dataset in $exprsVals and the probes chosen in $probeSets.  Otherwise, if returnProbes is \code{FALSE}, only the expression matrix is returned.
-#' @details If singleProbeset is set to \code{FALSE}, not recommended, the values for each sample will be taken from the maximum across any probe that maps to that gene.  This means that a gene's expression values may be a composition of values from different probes rather than a single probe.   if prefer is "up", when multiple deProbes match the same gene, the upregulated will be chosen.  Similary for "down".  Default is "none" and the probe with the oper will be chosen.
+#' @details If singleProbeset is set to \code{TRUE}, untested and not recommended, the values for each sample will be taken from the maximum across any probe that maps to that gene.  This means that a gene's expression values may be a composition of values from different probes rather than a single probe.   if prefer is "up", when multiple deProbes match the same gene, the upregulated will be chosen.  Similary for "down".  Default is "none" and the probe with the oper will be chosen.
 #' @author Christopher Bolen, Modified by Stefan Avey
 #' @keywords aveytoolkit
 #' @export
@@ -105,6 +105,8 @@ collapseDataset <- function(exprsVals, platform=NULL, mapVector=NULL, oper = max
 
   ## Reorder according to rownames (gene symbols) to be alphabetical
   exprMat <- rbind(exprsVals, maxOfProbes)
+  ## show(head(rownames(exprMat)))
+  ## show(head(exprMat))  
   reord <- order(rownames(exprMat))
   probeSets <- probeSets[reord]
   exprMat <- exprMat[reord,]
