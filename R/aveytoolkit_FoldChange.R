f8c395bcce62a5b44f83117bb8da238b8e58248a
spa23 <stefan.avey@yale.edu>
2014-09-03 16:51:38 -0400

    created function to automate taking the fold change between conditions which can get complicated when there are missing conditions! Tested and working on year 1 T/B cell data

--- R/aveytoolkit_FoldChange.R
+++ R/aveytoolkit_FoldChange.R
@@ -1,13 +1,13 @@
 #' FoldChange
 #'
-#' Calculate the fold change between pairs of conditions
+#' Calculate the fold change between pairs of conditions in a matrix or data frame
 #'
-#' @param hm a matrix of values used for drawing the heatmap
-#' @param pvals a list or data frame of (possibly FDR corrected but this is not handled by the function) positive p-values
-#' @param pvalDisplayName is printed on the heatmap as a legend.  Default is "P-value" but might want to change to "Q-value", "FDR", etc.
-#' @param cutoff is threshold for significance of pvals. Default is 0.05
-#' @param showOnly one of "both", "positive", "negative", or "all" can be abbreviated.
-#' @param main a string giving the plot main title. Default is "" (i.e. no title is plotted).
+#' @param x matrix or data.frame from which to calculate fold changes with samples in columns.
+#' @param condNum a vector of condition(s) to be used as the numerator in the fold change calculation
+#' @param condDen a vector of condition(s) to be used as the denominator in the fold change calculation
+#' @param conditions a vector with length equal to the number of columns of x containing the condition labels between which to find the fold changes.
+#' @param grouping a vector with length equal to the number of columns of x containing a grouping of the samples (e.g. subjects, cell lines, strains).
+#' @param log2Transform when 'TRUE', log2 transformation will be applied to x before taking the FC. If 'FALSE' (default) no transformation is applied and x is ASSUMED to be already log transformed.
 #' @return a data.frame of the fold changes with one column for each fold change
 #' @details
 #' @author Stefan Avey
@@ -15,24 +15,33 @@
 #' @export
 #' @examples
 
-FoldChange <- function(x, condNum, condDen, grouping, labels) {
-
-
-  exprFC <- data.frame(matrix(nrow=nrow(expr), ncol=ncol(expr) - 1/4 * ncol(expr)))
-  rownames(exprFC) <- rownames(expr)
-  lcv <- 1
-  for(sub in unique(target$SubjectID)) {
-    ## This works as long as the baseline measurement is not missing
-    if(0 %in% target$Time[target$SubjectID == sub]) {
-      ind <- grep(sub, colnames(expr))
-      exprFC[,lcv:(lcv+length(ind)-2)] <- expr[,ind[2:length(ind)]] - expr[,ind[1]]
-      colnames(exprFC)[lcv:(lcv+length(ind)-2)] <- colnames(expr)[ind[2:length(ind)]]
-      lcv <- lcv + length(ind)-1
+FoldChange <- function(x, condNum, condDen, conditions, grouping, log2Transform=FALSE) {
+  if(length(grouping) != ncol(x)) {
+    stop("grouping does not have the same number of elements as the columns (samples) of x.")
+  }
+  if(length(conditions) != ncol(x)) {
+    stop("conditions does not have the same number of elements as the columns (samples) of x.")    
+  }
+  if( !(all(c(condNum, condDen) %in% conditions)) ) {
+    stop("condNum or condDen contain values not in conditions.")
+  }
+  xFC <- data.frame(matrix(nrow=nrow(x), ncol=ncol(x)))
+  if(log2Transform) { x <- log2(x) }
+  ccv <- 1
+  for(g in unique(grouping)) {
+    group <- which(grouping == g)
+    for(cd in condDen) {
+      colD <- intersect(which(conditions == cd), group)
+      for(cn in condNum) {
+        colN <- intersect(which(conditions == cn), group)
+        if(length(colN) == 1 && length(colD) == 1) {
+          xFC[,ccv] <- x[,colN] - x[,colD]
+          colnames(xFC)[ccv] <- paste(g, paste(cn, cd, sep='-'), sep='.')
+          ccv <- ccv + 1 # incrememt column count variable
+        }
+      }
     }
   }
-
-
-
-
-
+  xFC <- Filter(function(a) !all(is.na(a)), xFC) # remove extra NA columns
+  return(xFC)
 }
