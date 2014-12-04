b0cf81631136cce2ea69c0a2de5bb3ed2c66be89
spa23 <stefan.avey@yale.edu>
2014-12-04 17:31:28 -0500

    Added block argument functionality (alpha)

--- R/aveytoolkit_runLimma.R
+++ R/aveytoolkit_runLimma.R
@@ -1,11 +1,11 @@
 ##' runLimma
 ##'
-##' This function performs a basic LIMMA analysis on the given expression set
+##' A wrapper around limma functions to perform a basic analysis on the given expression matrix
 ##'
 ##' @param eset the expression matrix
 ##' @param labels the labels for each column of the eset
 ##' @param contrasts Vector of contrasts to make
-##' @param block Vector of factors specifying a blocking variable (i.e. for paired samples or for ). NOT IMPLEMENTED!
+##' @param block vector or factor specifying a blocking variable on the arrays. Has length equal to the number of arrays. Must be ‘NULL’ if ‘ndups>2’. (Not extensively tested, use with caution)
 ##' @param covariates data frame of covariates (of same length as labels) to include in the model. Use this if there are paired samples, etc.
 ##' @param filterReplicateGenes Only include one probeset for each gene (determined by symbol)
 ##' @param min.fold.change Minimum log2 fold change to be differentially expressed. Default is 1.
@@ -13,9 +13,9 @@
 ##' @param p.cutoff FDR corrected cutoff for significant differential expression. Default is 0.05.
 ##' @param fitOnly If true, will return fit2, rather than the matrix of significant genes. Default is FALSE.
 ##' @return depends on \code{fitOnly}
-##' @details Generally, an expression matrix is made up of rows of genes (or any other features) and columns of samples. The matrix has data for multiple classes (which are denoted with the 'labels' parameter) and the classes are compared using the vector of contrasts.
+##' @details Generally, an expression matrix is made up of rows of genes (or any other features) and columns of samples. The matrix has data for multiple classes (which are denoted with the 'labels' parameter) and the classes are compared using the vector of contrasts. Block can be used for biological (or technical) replicates or for separate subjects (in which case it will determinte the inter-subject correlation).  See \code{?duplicateCorrelation} for more information.
 ##' @import limma
-##' @author Christopher Bolen
+##' @author Christopher Bolen, Stefan Avey
 ##' @keywords aveytoolkit
 ##' @seealso \code{\link{limma}}
 ##' @export
@@ -32,17 +32,10 @@
 ##' ## If you want information on p-values and fold changes, set "fitOnly=T", and you can access
 ##' ## the fit object to get the information.
 ##' ## 
-##' ## For other comparisons, you can look at the LIMMA user guide.
-runLimma  <- function(eset,
-                      labels,                           
-                      contrasts,                        
-                      ## block = NULL,                  
-                      covariates=NULL,                  
-                      filterReplicateGenes = TRUE,      
-                      min.fold.change = 1,              
-                      min.intensity = 4,                
-                      p.cutoff = 0.05,                  
-                      fitOnly = FALSE)                   
+##' ## For other comparisons, you can look at the LIMMA user guide: limmaUsersGuide()
+runLimma  <- function(eset, labels, contrasts, block = NULL, covariates=NULL,
+                      filterReplicateGenes = TRUE, min.fold.change = 1, min.intensity = 4,
+                      p.cutoff = 0.05, fitOnly = FALSE)                   
 {                                                                      
   ##trim eset
   tooLow =  eset < min.intensity
@@ -62,21 +55,22 @@ runLimma  <- function(eset,
      covariates[,i] = as.factor(covariates[,i])
      cns = c(cns, paste("cov",i,levels(covariates[,i])[-1],sep="."))
    }
-   attach(covariates, warn.conflicts=F)
+   attach(covariates, warn.conflicts=FALSE)
    design <- model.matrix(formula(f))
    colnames(design) = cns
    detach(covariates)
   }
-  #cor = NA
-  #if(!is.null(block)){
-  #  block = as.factor(block)
-  #  corfit = duplicateCorrelation(eset, design=design, block=block)
-  #  cor = corfit$consensus
-  #}
-  
-  fit <- lmFit(eset, design=design)#, cor=cor, block=block)
-  rownames(fit$coefficients) <- rownames(eset)             ## For some unknown reason, using block causes rownames to disappear
-  rownames(fit$stdev.unscaled) <- rownames(eset)           ## For some unknown reason, using block causes rownames to disappear
+  cor <- NULL
+  if(!is.null(block)){
+   block <- as.factor(block)
+   corfit <- duplicateCorrelation(eset, design=design, block=block)
+   cor <- corfit$consensus.correlation
+  }
+  fit <- lmFit(eset, design=design, block=block, correlation=cor)
+  ## fit <- lmFit(eset, design=design)#, cor=cor, block=block)  
+  ## For some unknown reason, using block causes rownames to disappear
+  rownames(fit$coefficients) <- rownames(eset) 
+  rownames(fit$stdev.unscaled) <- rownames(eset)           
   
   contrast.matrix <- makeContrasts( contrasts=contrasts, levels=design)
   fit2 <- contrasts.fit(fit,contrast.matrix)
@@ -90,38 +84,3 @@ runLimma  <- function(eset,
   }
   
 }
-
-## ## My own homebrewed version of the Volcano Plot that limma has.
-## ## If you run Limma with "fitOnly=T", you can simply stick the fit object from the function
-## ## into this one and it will produce a volcano plot from the first comparison that you ran.
-## ## To change what comparison you make the plot for, set "coef" to the number of the comparison.
-## volcanoPlot = function(fit, 
-##                        coef=1,
-##                        colored=T,
-##                        min.lfc=1,
-##                        p.cutoff=0.05,
-##                        convertToFDR=T,
-##                        xlab="Log Fold Change",
-##                        ylab=NA,
-##                        pch=16,
-##                        cex=0.35,
-##                        ...
-##                        ){
-##   lfc = fit$coefficients[,coef]
-##   pval = fit$p.value[,coef]
-##   if(convertToFDR){
-##     pval = p.adjust(pval, "fdr")
-##     if(is.na(ylab)){ylab="-log10(False Discovery Rate)"}
-##   }
-##   if(is.na(ylab)){ylab="-log10(P Value)"}
-  
-  
-##   if(!is.na(p.cutoff) & !is.na(min.lfc)){
-##     cols = c("grey","dark grey","red")[(abs(lfc)>min.lfc) + (pval<p.cutoff) + 1]
-##   }else{cols="black"}
-##   plot(lfc, -log10(pval), col=cols, xlab=xlab,ylab=ylab,pch=pch,cex=cex,...)
-##   abline(h=-log10(p.cutoff))
-##   abline(v=min.lfc)
-##   abline(v=-min.lfc)
-## }
-
