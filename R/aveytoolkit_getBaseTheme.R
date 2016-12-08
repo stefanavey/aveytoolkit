22c8b3f0f86fac899b44147b8690f21301b05403
Stefan Avey <stefan.avey@yale.edu>
2016-12-08 11:53:01 -0500

    Modified base theme

--- R/aveytoolkit_getBaseTheme.R
+++ R/aveytoolkit_getBaseTheme.R
@@ -17,12 +17,13 @@ getBaseTheme <- function( ) {
   ## Define universal plot settings
   base_theme <- theme_bw() +
     theme(text=element_text(size=14)) +
-      theme(plot.title=element_text(size=16)) +
-        theme(strip.background=element_rect(fill='white')) +
-          theme(strip.text=element_text(size=16, face='bold'))
-  ## theme(axis.title=element_text(size=16, vjust=0.5)) +
-  ##   theme(axis.text.x=element_text(size=14, vjust=0.5, hjust=0.5)) +
-  ##     theme(axis.text.y=element_text(size=14))
+    theme(plot.title=element_text(size=16)) +
+    theme(strip.background=element_rect(fill='white')) +
+    theme(strip.text=element_text(size=16, face='bold')) +
+    theme(plot.title = element_text(hjust = 0.5))
+    ## theme(axis.title=element_text(size=16, vjust=0.5)) +
+    ## theme(axis.text.x=element_text(size=14, vjust=0.5, hjust=0.5)) +
+    ## theme(axis.text.y=element_text(size=14))
   return(base_theme)
 }
 
