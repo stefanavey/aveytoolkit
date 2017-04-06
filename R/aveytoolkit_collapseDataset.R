b0d645b23883364fb7e150d62ba3ee92d7fc701d
Stefan Avey <stefan.avey@yale.edu>
2017-04-06 13:46:47 -0400

    Added warning to collapseDataset function because it doesn't handle a mapVector with non-unique names

--- R/aveytoolkit_collapseDataset.R
+++ R/aveytoolkit_collapseDataset.R
@@ -4,7 +4,7 @@
 ##'
 ##' @param exprsVals a matrix or data.frame of numeric values with rownames denoting the identifiers.
 ##' @param platform the microarray platform the data comes from for extracting the gene symbols
-##' @param mapVector a named character vector with names specififying the current identifiers (probes matching the rownames of exprsVals) and the values of the vector specifying the gene symbols (or other identifier to collapse to). 
+##' @param mapVector a uniquely named character vector with names specififying the current identifiers (probes matching the rownames of exprsVals) and the values of the vector specifying the gene symbols (or other identifier to collapse to). 
 ##' @param oper the operation used to choose which probe when multiple probes map to the same gene.  Default is max which will calculate the maximum of the average.
 ##' @param prefer one of "none", "up", or "down", can be abbreviated.
 ##' @param singleProbeset If \code{TRUE}, the operation applies to the average over all conditions and all values for a gene will come from one probeset. Otherwise, if \code{FALSE}, the operation applies to the probesets over all conditions and the values for a gene may come from different probe sets . Default is \code{FALSE} for compatability reasons but \code{TRUE} is recommended.
@@ -45,9 +45,12 @@ collapseDataset <- function(exprsVals, platform=NULL, mapVector=NULL, oper = max
 {
   if(is.null(platform) && is.null(mapVector)){
     stop("Need to include either a platform or a named vector to map to")
-  }                              
+  }
+  if(any(duplicated(names(mapVector)))) {
+    warning("`mapVector` contains duplicate names. Results may be incorrect. Please remove duplicates.")  
+  }
   if(!is.null(platform)){
-                                        #find the right columns
+    ## find the right columns
     if("Gene.Symbol" %in% names(platform)){ mapVector = as.vector(platform$Gene.Symbol)}
     else if("GeneSymbol" %in% names(platform)){ mapVector = as.vector(platform$GeneSymbol)}
     else if("SYMBOL" %in% names(platform)){ mapVector = as.vector(platform$SYMBOL)} 
@@ -63,20 +66,18 @@ collapseDataset <- function(exprsVals, platform=NULL, mapVector=NULL, oper = max
   geneSymbols = mapVector[probeSets]
   allDEprobes <- Reduce(union, deProbes)
   
-                                        #remove probes with NO gene symbol
+  ## remove probes with NO gene symbol
   probeSets_NoGeneSymbol = ((geneSymbols == "") | is.na(geneSymbols))
   exprsVals = as.matrix(exprsVals[ !probeSets_NoGeneSymbol,])
   probeSets = probeSets[!probeSets_NoGeneSymbol]
   geneSymbols = geneSymbols[!probeSets_NoGeneSymbol]
   
-                                        #remove rows that contain NAs
+  ## remove rows that contain NAs
   missingData = apply(is.na(exprsVals), 1,any)
   exprsVals = as.matrix(exprsVals[ !missingData,])
   probeSets = probeSets[!missingData]
   geneSymbols = geneSymbols[!missingData]
 
-  ## browser() ## debugging
-  
   ## combine all the probes for genes with more than one name using the following function
   geneSymbols_Multiple =  names(which(table(geneSymbols)>1))
   maxOfProbes = matrix(NA,nrow=length(geneSymbols_Multiple),ncol=dim(exprsVals)[2],
@@ -103,11 +104,7 @@ collapseDataset <- function(exprsVals, platform=NULL, mapVector=NULL, oper = max
         if(length(filter) > 0)
           probes <- probes[filter, , drop=F]
       }
-      ## print(matchingProbes)
-      ## print(matchingProbes.de)
     }
-    ## print(probes)
-    ## print(g)
     if (! singleProbeset ) {
       maxOfProbes[g,] = apply(probes,2,oper)
     } else { # only choose a single probeset for each duplicate
@@ -118,7 +115,6 @@ collapseDataset <- function(exprsVals, platform=NULL, mapVector=NULL, oper = max
         maxOfProbes[g,] = probes[whichMax,]
         maxProbeNames[g] <- rownames(probes)[whichMax]
       }
-    ## Pause()
   }
 
   geneSymbols_Multiple = which( geneSymbols %in% geneSymbols_Multiple )
@@ -134,15 +130,13 @@ collapseDataset <- function(exprsVals, platform=NULL, mapVector=NULL, oper = max
 
   ## Reorder according to rownames (gene symbols) to be alphabetical
   exprMat <- rbind(exprsVals, maxOfProbes)
-  ## show(head(rownames(exprMat)))
-  ## show(head(exprMat))  
   reord <- order(rownames(exprMat))
   probeSets <- probeSets[reord]
   exprMat <- exprMat[reord,]
 
-  if(returnProbes) # Return probes and the expression matrix
+  ## Return probes and expression or only expression
+  if(returnProbes) 
     return(list(exprsVals=exprMat, probes=probeSets))
-  else # return only the expression matrix
+  else
     return(exprMat)
 }
-
