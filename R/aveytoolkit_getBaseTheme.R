4e2472cc0ee35558d23526e869e7eb89be508252
spa23 <stefan.avey@yale.edu>
2017-11-17 12:40:31 -0500

    Added caption option and fixed typo in getBaseTheme function

--- R/aveytoolkit_getBaseTheme.R
+++ R/aveytoolkit_getBaseTheme.R
@@ -5,7 +5,7 @@
 ##'
 ##' @import ggplot2
 ##' 
-##' @author Jason Vander Heiden <jason.vanderheiden@@yale.edu> 
+##' @author Jason Vander Heiden <jason.vanderheiden@@yale.edu>, Stefan Avey <stefan.avey@yale.edu>
 ##' @keywords aveytoolkit
 ##' @export
 ##' @examples
@@ -14,17 +14,18 @@
 ##' plot(p)
 
 getBaseTheme <- function( ) {
-  ## Define universal plot settings
-  base_theme <- theme_bw() +
-    theme(text=element_text(size=14)) +
-    theme(plot.title=element_text(size=18)) +
-    theme(strip.background=element_rect(fill='white')) +
-    theme(strip.text=element_text(size=16, face='bold')) +
-    theme(plot.title = element_text(hjust = 0.5))
-    theme(axis.title=element_text(size=16, vjust=0.5)) +
-    ## theme(axis.text.x=element_text(size=14, vjust=0.5, hjust=0.5)) +
-    ## theme(axis.text.y=element_text(size=14))
-  return(base_theme)
+    ## Define universal plot settings
+    base_theme <- theme_bw() +
+        theme(text=element_text(size=14)) +
+        theme(plot.title=element_text(size=18)) +
+        theme(strip.background=element_rect(fill='white')) +
+        theme(strip.text=element_text(size=16, face='bold')) +
+        theme(plot.title = element_text(hjust = 0.5)) +
+        ## theme(axis.text.x=element_text(size=14, vjust=0.5, hjust=0.5)) +
+        ## theme(axis.text.y=element_text(size=14)) +
+        theme(axis.title=element_text(size=16, vjust=0.5)) +
+        theme(plot.caption = element_text(size = 10, face = "italic"))
+    return(base_theme)
 }
 
 
