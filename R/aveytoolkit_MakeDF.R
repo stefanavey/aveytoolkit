739f0016fbe868af9d11cc1200c20e2a12441db6
Stefan Avey <stefanavey@Stefans-MacBook-Air.local>
2020-03-04 14:09:37 -0500

    don't export MakeDF function which doesn't seem to work

--- R/aveytoolkit_MakeDF.R
+++ R/aveytoolkit_MakeDF.R
@@ -3,27 +3,17 @@
 ##' Creates a data frame from a list.  Useful for when the list elements have unequal lengths and
 ##' \code{\link{as.data.frame}} fails.
 ##'
-##' @param list the list to convert
-##' @param names the names of the list
+##' @param List the list to convert
+##' @param Names the names of the list
 ##' @return a data frame of the converted list.
 ##' @keywords aveytoolkit
 ##' @seealso \code{\link{gsub}}
 ##' @references \url{http://stackoverflow.com/questions/15753091/convert-mixed-length-named-list-to-data-frame}
 ##' @author Josh O'Brien
-##' @export
-##' @examples
-##' ## Test timing with a 50k-item list
-##' ll <- createList(50000)
-##' nms <- c("a", "b", "c")
-##' 
-##' system.time(makeDF(ll, nms))
-##' # user  system elapsed
-##' # 0.47    0.00    0.47 
-
-MakeDF <- function(list, names) {
-  m <- t(vapply(list,
-                FUN = function(X) unlist(X)[names],
-                FUN.VALUE = numeric(length(names))))
-  as.data.frame(m)
+## TODO: Fix this function, doesn't seem to work
+MakeDF <- function(List, Names) {
+    m <- t(vapply(List, 
+                  FUN = function(X) unlist(X)[Names], 
+                  FUN.VALUE = numeric(length(Names))))
+    as.data.frame(m)
 }
-
--- R/aveytoolkit_cbindFill.R
+++ R/aveytoolkit_cbindFill.R
@@ -1,22 +1,23 @@
-#' resetPar
-#'
-#' Simple function to combine multiple objects by column while filling in NAs into extra rows created from differing lengths
-#'
-#' @param ... the objects, that will be converted to a list, to bind column-wise
-#' @return the cbind'ed objects passed in
-#' @author Dimitris Rizopoulos and Tyler Rinker
-#' @keywords aveytoolkit
-#' @seealso \code{\link{cbind}}
-#' @references \url{http://stackoverflow.com/questions/7962267/cbind-a-df-with-an-empty-df-cbind-fill}
-#' @export
-#' @examples
-#' x<-matrix(1:10,5,2)
-#' y<-matrix(1:16, 4,4)
-#' z<-matrix(1:12, 2,6)
-#' 
-#' cbind.fill(x,y)
-#' cbind.fill(x,y,z)
-#' cbind.fill(mtcars, mtcars[1:10,])
+##' resetPar
+##'
+##' Simple function to combine multiple objects by column while filling in NAs into extra rows created from differing lengths
+##'
+##' @param ... the objects, that will be converted to a list, to bind column-wise
+##' @return the cbind'ed objects passed in
+##' @author Dimitris Rizopoulos and Tyler Rinker
+##' @keywords aveytoolkit
+##' @seealso \code{\link{cbind}}
+##' @references \url{http://stackoverflow.com/questions/7962267/cbind-a-df-with-an-empty-df-cbind-fill}
+##' @export
+##' @examples
+##' 
+##' x <- matrix(1:10, 5, 2)
+##' y <- matrix(1:16, 4, 4)
+##' z <- matrix(1:12, 2, 6)
+##' 
+##' cbind.fill(x,y)
+##' cbind.fill(x,y,z)
+##' cbind.fill(mtcars, mtcars[1:10,])
 cbind.fill <- function(...) {
   nm <- list(...)
   nm <- lapply(nm, as.matrix)
